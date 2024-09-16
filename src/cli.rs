7e15285e8ed89dd0a30b8610dc28d7a3cdab695e
Tommi Mäklin <tommi@maklin.fi>
2024-09-16 18:32:49 +0300

    Implement CLI placeholders for build and map.

---
+++ src/cli.rs
@@ -0,0 +1,58 @@
+// sablast: Spectral Burrows-Wheeler transform accelerated local alignment search
+//
+// Copyright 2024 Tommi Mäklin [tommi@maklin.fi].
+
+// Copyrights in this project are retained by contributors. No copyright assignment
+// is required to contribute to this project.
+
+// Except as otherwise noted (below and/or in individual files), this
+// project is licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE> or <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
+// at your option.
+//
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    // Build SBWT index
+    Build {
+        // Input fasta or fastq sequence file(s)
+        #[arg(group = "input", required = true)]
+        input: Vec<String>,
+
+	// Input sequence list
+        #[arg(short = 'l', long = "input-list", group = "input", required = true)]
+        input_list: Option<String>,
+
+	// Outputs
+        #[arg(short = 'o', long = "out-prefix", required = false, help_heading = "Output")]
+        output_prefix: Option<String>,
+
+        // Resources
+	// Threads
+        #[arg(short = 't', long = "threads", default_value_t = 1)]
+        num_threads: usize,
+	// Memory in GB
+        #[arg(short = 'm', long = "memory", default_value_t = 4)]
+        mem_gb: usize,
+	// Temporary directory
+        #[arg(long = "tmp-dir", required = false)]
+        temp_dir: Option<String>,
+
+        #[arg(long = "verbose", default_value_t = false)]
+        verbose: bool,
+    },
+
+    // Map query against SBWT index
+    Map {
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+// sablast: Spectral Burrows-Wheeler transform accelerated local alignment search
+//
+// Copyright 2024 Tommi Mäklin [tommi@maklin.fi].
+
+// Copyrights in this project are retained by contributors. No copyright assignment
+// is required to contribute to this project.
+
+// Except as otherwise noted (below and/or in individual files), this
+// project is licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE> or <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
+// at your option.
+//
+use clap::Parser;
+use log::{info, Record, Level, Metadata};
+
+// Command-line interface
+mod cli;
+
+// Logger implementation
+struct Logger;
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+fn init_log(log_max_level: usize) {
+    stderrlog::new()
+	.module(module_path!())
+	.quiet(false)
+	.verbosity(log_max_level)
+	.timestamp(stderrlog::Timestamp::Off)
+	.init()
+	.unwrap();
+}
+
+// Use `sablast` to list the available commands or `sablast <command>` to run.
 fn main() {
-    println!("Hello, world!");
+    let cli = cli::Cli::parse();
+
+    // Subcommands:
+    match &cli.command {
+        // Run the full pipeline
+        Some(cli::Commands::Build {
+	    input,
+	    input_list,
+	    output_prefix,
+	    num_threads,
+	    mem_gb,
+	    temp_dir,
+	    verbose,
+        }) => {
+	    init_log(if *verbose { 2 } else { 1 });
+	    info!("Building SBWT index");
+        },
+        Some(cli::Commands::Map {
+        }) => todo!(),
+	None => {}
+    }
 }
