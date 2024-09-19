48d05a384fadb0c292a819c137a5bd72305786bb
Tommi Mäklin <tommi@maklin.fi>
2024-09-19 21:56:37 +0300

    Remove unused logger implementation.

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,10 @@
 use std::ffi::OsString;
 
 use clap::Parser;
-use log::{info, Record, Level, Metadata};
+use log::info;
 
 // Command-line interface
 mod cli;
-
-// Logger implementation
-struct Logger;
-
-impl log::Log for Logger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Info
-    }
-
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
-        }
-    }
-
-    fn flush(&self) {}
-}
 
 fn init_log(log_max_level: usize) {
     stderrlog::new()
