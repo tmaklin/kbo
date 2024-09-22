cee4b2ac5b942cbd05d4244f977fa5953615f21f
Tommi Mäklin <tommi@maklin.fi>
2024-09-22 20:34:37 +0300

    Update integration test map_clbs.rs

--- tests/map_clbs.rs
+++ tests/map_clbs.rs
@@ -1,9 +1,26 @@
+// sablast: Spectral Burrows-Wheeler transform accelerated local alignment search
+//
+// Copyright 2024 Tommi Mäklin [tommi@maklin.fi].
+
+// Copyrights in this project are retained by contributors. No copyright assignment
+// is required to contribute to this project.
+
+// Except as otherwise noted (below and/or in individual files), this
+// project is licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE> or <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
+// at your option.
+//
 #[test]
 fn map_nissle_against_clbs() {
     let (sbwt, lcs) = sablast::index::build_sbwt(&"tests/data/clbS.fna.gz".to_string(), &None);
 
     let expected = vec![(455, 967, 512, 1, '+'),(997, 1001, 5, 0, '+'),(998, 1001, 4, 0, '-')];
-    let got = sablast::map(&"tests/data/NZ_CP058217.1_clbS.fna.gz".to_string(), &sbwt::SbwtIndexVariant::SubsetMatrix(sbwt), &lcs.unwrap());
+    let aln = sablast::map(&"tests/data/NZ_CP058217.1_clbS.fna.gz".to_string(), &sbwt::SbwtIndexVariant::SubsetMatrix(sbwt), &lcs.unwrap());
+
+    let mut got: Vec<(usize, usize, usize, usize, char)> = sablast::format::run_lengths(&aln.0).iter().map(|x| (x.0, x.1, x.2, x.3, '+')).collect();
+    let mut run_lengths_rev: Vec<(usize, usize, usize, usize, char)> = sablast::format::run_lengths(&aln.1).iter().map(|x| (x.0, x.1, x.2, x.3, '-')).collect();
+    got.append(&mut run_lengths_rev);
 
     assert_eq!(got, expected);
 }
