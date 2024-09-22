280e4a351b9fdaed0538ef8222e38c3935da6af1
Tommi Mäklin <tommi@maklin.fi>
2024-09-22 20:01:44 +0300

    Update test values.

--- tests/map_clbs.rs
+++ tests/map_clbs.rs
@@ -2,7 +2,7 @@
 fn map_nissle_against_clbs() {
     let (sbwt, lcs) = sablast::index::build_sbwt(&"tests/data/clbS.fna.gz".to_string(), &None);
 
-    let expected = vec![(455, 967, 512, 1, '+'),(996, 1001, 4, 2, '+'),(998, 1000, 3, 0, '-')];
+    let expected = vec![(455, 967, 512, 1, '+'),(997, 1001, 5, 0, '+'),(998, 1001, 4, 0, '-')];
     let got = sablast::map(&"tests/data/NZ_CP058217.1_clbS.fna.gz".to_string(), &sbwt::SbwtIndexVariant::SubsetMatrix(sbwt), &lcs.unwrap());
 
     assert_eq!(got, expected);
