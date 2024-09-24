249743ea19267e4dbd111433f49edf9e4533f249
Tommi Mäklin <tommi@maklin.fi>
2024-09-24 11:31:16 +0300

    Add documentation to build.

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,54 @@ pub mod format;
 pub mod index;
 pub mod translate;
 
+/// Builds an SBWT index from some fasta or fastq files.
+///
+/// Reads all sequence data in `seq_files` and builds an SBWT index
+/// with the parameters and resources specified in `build_opts` (see
+/// [index::BuildOpts] for details).
+///
+/// All files and sequence data in `seq_files` are merged into the
+/// same index. It is not possible extract the individual sequences
+/// from the index after it has been built; use [map] with the TODO
+/// options instead if you need to know which reference sequences the
+/// alignments are for.
+///
+/// TODO Describe map syntax in lib.rs documentation.
+///
+/// Returns a tuple containing the built
+/// [sbwt::SbwtIndexVariant](https://docs.rs/sbwt/latest/sbwt/enum.SbwtIndexVariant.html)
+/// and
+/// [sbwt::LcsArray](https://docs.rs/sbwt/latest/sbwt/struct.LcsArray.html).
+///
+/// Panics if a file in `seq_files` is not readable or a valid FASTX
+/// file.
+///
+/// # Input format detection
+/// The sequence data is read using
+/// [needletail::parser::parse_fastx_file](https://docs.rs/needletail/latest/needletail/parser/fn.parse_fastx_file.html).
+///
+/// Input file format (fasta or fastq) is detected automatically and
+/// the files may be compressed in a
+/// [DEFLATE-based](https://en.wikipedia.org/wiki/Deflate) format (.gz
+/// files).
+///
+/// [Bzip2](https://sourceware.org/bzip2/) and
+/// [liblzma](https://tukaani.org/xz/) compression (.bz2 and .xz
+/// files) can be enabled using the needletail features field in
+/// sablast Cargo.toml if compiling from source.
+///
+/// # Examples
+/// ```rust
+/// use sablast::build;
+/// use sablast::index::BuildOpts;
+///
+/// let inputs = vec!["tests/data/clbS.fna.gz".to_string(), "tests/data/NZ_CP058217.1_clbS.fna.gz".to_string()];
+///
+/// let (sbwt_index, lcs_array) = build(&inputs, BuildOpts::default());
+/// ```
+///
 pub fn build(
-    seq_files: &Vec<String>,
+    seq_files: &[String],
     build_opts: index::BuildOpts,
 ) -> (sbwt::SbwtIndexVariant, sbwt::LcsArray) {
     let mut seq_data: Vec<Vec<u8>> = Vec::new();
