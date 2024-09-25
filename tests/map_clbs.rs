5a8d8c7dec9ca43c1d62c4f96d2b977f14de0219
Tommi Mäklin <tommi@maklin.fi>
2024-09-25 14:14:29 +0300

    Update

--- tests/map_clbs.rs
+++ tests/map_clbs.rs
@@ -15,7 +15,19 @@
 fn map_nissle_against_clbs() {
     use needletail::Sequence;
 
-    let (sbwt, lcs) = sablast::index::build_sbwt_from_file(&"tests/data/clbS.fna.gz".to_string(), &None);
+    let mut seq_data: Vec<Vec<u8>> = Vec::new();
+    let mut reader = needletail::parse_fastx_file("tests/data/clbS.fna.gz".to_string()).unwrap_or_else(|_| panic!("Expected valid fastX file at tests/data/clbS.fna.gz"));
+    loop {
+	let rec = reader.next();
+	match rec {
+	    Some(Ok(seqrec)) => {
+		seq_data.push(seqrec.normalize(true).as_ref().to_vec());
+	    },
+	    _ => break
+	}
+    }
+
+    let (sbwt, lcs) = sablast::index::build_sbwt_from_vecs(&seq_data, &None);
 
     let expected = vec![(455, 967, 512, 1)];
 
