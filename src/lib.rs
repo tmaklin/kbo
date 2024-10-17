45477b6986d1f21289926e9f6f87961a260ceca7
Tommi Mäklin <tommi@maklin.fi>
2024-10-17 15:35:50 +0300

    Add extra warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,13 @@
 // the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
 // at your option.
 //
+#![warn(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
 use sbwt::SbwtIndexVariant;
 
 pub mod derandomize;
